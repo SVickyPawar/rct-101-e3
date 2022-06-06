c523d62004c07d29bc3f3628de74e03bbd84a92e
SVickyPawar <97332040+SVickyPawar@users.noreply.github.com>
2022-06-06 16:58:31 +0530

    second commit

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import {Product} from "../Products/Product"
 
 const Products = () => {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchinfo=async()=>{
+      await fetch("http://localhost:8080/products").then(r=>r.json()).then((d)=>{setData(d)});
+    }
+    fetchinfo()
+   
+  }, [])
+  
+  
   return <div>{/* Code here */}
   <Product/>
   </div>;
